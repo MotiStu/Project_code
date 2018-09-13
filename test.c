77981d255b67cf185065f5d12160ed3cc0491488
MotiStu <43229946+MotiStu@users.noreply.github.com>
2018-09-13 14:00:13 +0800

    Create test.c

---
+++ test.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "Snake.h"
+#include <string.h>
+#include <Windows.h>
+
+void GameStart(pSnake ps)
+{
+	WelcomeToGame();            //欢迎界面
+	CreateMap();					//设置地图
+	InitSnake(ps);
+	CreateFood(ps);
+	ps->_Sleeptime = 200;
+	ps->_totalScore = 0;
+	ps->_AddScore = 10;
+	ps->_Sta = OK;
+	ps->_Dir = RIGHT;
+}
+
+
+
+
+int main()
+{
+	system("color 3F");
+	Snake snake;
+	srand((unsigned)time(NULL));
+	
+	GameStart(&snake);           //游戏开始
+	GameRun(&snake);             //运行游戏
+	GameEnd(&snake);             //游戏结束
+
+	
+	system("pause");
+	return 0;
+}
